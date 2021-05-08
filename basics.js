193384485a9fb71e89cd045ab815d34741bbb1a3
Ian <iansedano@gmail.com>
2021-05-08 17:30:30 +0200

    having fun with rects

--- basics.js
+++ basics.js
@@ -2,11 +2,20 @@ const canvas = document.getElementById("canvas");
 
 const context = canvas.getContext('2d');
 
-context.fillStyle = 'rgb(200, 0, 0)' // can also be rgba
-context.fillRect(10, 10, 280, 280); // x, y, width, height
+context.fillStyle = '#272822'
+context.fillRect(0, 0, 300, 300); // x, y, width, height
 
-context.clearRect(50, 50, 50, 50);
 
-context.strokeRect(100, 100, 50, 50);
-context.strokeRect(110, 110, 50, 50);
-context.strokeRect(120, 120, 50, 50);
+context.strokeStyle = 'rgb(122, 123, 117)'
+let r = 150
+let g = 150
+let b = 117 
+
+for (let i = 0; i != 250; i+=5){
+    context.strokeRect(i, i, 50, 50);
+    r-=1
+    g-=2
+    b-=3
+    context.strokeStyle = `rgb(${r}, ${g}, ${b})`
+}
+

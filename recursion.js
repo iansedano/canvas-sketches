591ed491c8ca600e65a25e6e1e58bf8021be5f52
Ian <iansedano@gmail.com>
2021-08-01 09:31:20 +0200

    messing around with tree function

--- recursion.js
+++ recursion.js
@@ -9,20 +9,43 @@ draw.background("grey")
 
 const GOLDEN_RATIO = 1.61803398875
 let angle = -Math.PI / 2
-const startingLength = draw.height/3
+const startingLength = draw.height * 2
 const startPoint = new Point(draw.width/2, draw.height-50)
 const startingThickness = 10
+let maxBranchNum = 0
 
-function branch(point, length, angle, thickness) {
-    if (length < 1) return
+function branch(point, length, angle, thickness, branchNum = 1) {
+    if (maxBranchNum < branchNum) maxBranchNum = branchNum
+    if (length < 2) return
+    
+    const branchLengthCoefficient = -8.5 * Math.log(branchNum + 1) + 20
+    const clampedBranchLengthCoefficient = branchLengthCoefficient < 1 ? 1 : branchLengthCoefficient
+    const branchLength = length * (1 / clampedBranchLengthCoefficient)
+    
     const endBranch = new Point(
-        point.x + Math.cos(angle) * length,
-        point.y + Math.sin(angle) * length)
+            point.x + Math.cos(angle) * branchLength,
+            point.y + Math.sin(angle) * branchLength)
+        
     draw.line(point, endBranch, undefined , thickness)
     
-    branch(endBranch, length / GOLDEN_RATIO, angle + 0.5, thickness / GOLDEN_RATIO)
-    branch(endBranch, length / GOLDEN_RATIO, angle - 0.2, thickness / GOLDEN_RATIO)
-
+    const branchAngles = [
+        0.3,
+        -0.3
+    ]
+    branchAngles
+        .map(a => {
+            return a * ((Math.random() + 0.5) / 1.5)
+        })
+        .forEach(a => {
+            branch(
+                endBranch,
+                length / GOLDEN_RATIO + (Math.random() - 0.5),
+                angle + a,
+                thickness / GOLDEN_RATIO,
+                branchNum + 1
+            )
+        })
 }
 
 branch(startPoint, startingLength, angle, startingThickness)
+console.log(maxBranchNum)

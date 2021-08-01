831ea42ba8b1034b55f78eb13bc490a722b401b8
Ian <iansedano@gmail.com>
2021-08-01 09:33:17 +0200

    simpler messing around

--- recursion.js
+++ recursion.js
@@ -9,7 +9,7 @@ draw.background("grey")
 
 const GOLDEN_RATIO = 1.61803398875
 let angle = -Math.PI / 2
-const startingLength = draw.height * 2
+const startingLength = draw.height/1.5
 const startPoint = new Point(draw.width/2, draw.height-50)
 const startingThickness = 10
 let maxBranchNum = 0
@@ -18,33 +18,33 @@ function branch(point, length, angle, thickness, branchNum = 1) {
     if (maxBranchNum < branchNum) maxBranchNum = branchNum
     if (length < 2) return
     
-    const branchLengthCoefficient = -8.5 * Math.log(branchNum + 1) + 20
-    const clampedBranchLengthCoefficient = branchLengthCoefficient < 1 ? 1 : branchLengthCoefficient
-    const branchLength = length * (1 / clampedBranchLengthCoefficient)
-    
-    const endBranch = new Point(
-            point.x + Math.cos(angle) * branchLength,
-            point.y + Math.sin(angle) * branchLength)
+    let endBranch
+    if (branchNum > 4) {
+        endBranch = new Point(
+            point.x + Math.cos(angle) * length,
+            point.y + Math.sin(angle) * length)
+    } else {
+        endBranch = new Point(
+            point.x + Math.cos(angle) * (length / 3),
+            point.y + Math.sin(angle) * (length / 3))
+    }
         
     draw.line(point, endBranch, undefined , thickness)
     
     const branchAngles = [
-        0.3,
-        -0.3
+        Math.random() / 2,
+        Math.random() / -2,
+        0
     ]
-    branchAngles
-        .map(a => {
-            return a * ((Math.random() + 0.5) / 1.5)
-        })
-        .forEach(a => {
-            branch(
-                endBranch,
-                length / GOLDEN_RATIO + (Math.random() - 0.5),
-                angle + a,
-                thickness / GOLDEN_RATIO,
-                branchNum + 1
-            )
-        })
+    branchAngles.forEach(a => {
+        branch(
+            endBranch,
+            length / GOLDEN_RATIO + (Math.random() - 0.5),
+            angle + (a * (Math.random()*2)),
+            thickness / GOLDEN_RATIO,
+            branchNum + 1
+        )
+    })
 }
 
 branch(startPoint, startingLength, angle, startingThickness)
